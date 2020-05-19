cc54a97e9e13312f59d20d3004264cf36f031959
Nehal Kalnad <yenwiikae@gmail.com>
2020-05-19 13:02:21 +0530

    add graph ui and read me

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-maha_covid_api
+# Maha-Covid-Api
+#
+
+Maha-Covid-Api is API for Maharashtra Corona Cases with district wise data. This data is not widely available in other APIs or Website. This data is upload in PDF report format by twitter account of [ Medical Education & Drugs Department, Maharashtra ](https://twitter.com/Maha_MEDD) ( [sample report]( https://drive.google.com/file/d/1PjkafyLnCxLh5ul-LSeHckhqfMXzOf40/view ) ) 
+
+
+# Data Extraction :
+  - Track daily tweets of @Maha_MEDD account with key words "A daily comprehensive report prepared by MEDD, Maharashtra showing #COVIDー19 situation in the state" 
+  - Extract the Google Drive url of pdf from tweet and download the pdf on server
+  - Extract the Report Table from Pdf using [Tabula]( https://github.com/chezou/tabula-py )
+  - Apply Data Cleaning to get useful data
+  - Update the database
+
+
+
+### Installation
+
+Dillinger requires Python 3.X+ to run.
+Create a virtual env using Conda or virtualenv install the dependencies and start the server.
+
+```sh
+$ cd maha_covid_api
+$ pip install requirements.txt
+$ python app.py
+```
+
+
+
+### API
+#
+
+
+| PATH | METHOD | DESCRIPTION |
+| ------ | ------ | ------ |
+| /latest | GET | Get the latests statistics of all districts/muncipals |
+| /ids | GET | Get ids of all districts/muncipals to use for history API |
+| /all | GET | Get all information from database
+| /history/<id> | GET | Get past 14 days information of particular districts/muncipals  |
+
+
--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import flask
 import os
-from flask import jsonify, request
+from flask import jsonify, request , make_response
 from flask import flash, redirect, url_for, session , render_template
 from flask_cors import CORS, cross_origin
 import requests, json
 import pandas as pd
 import requests
-
+import json 
+from datetime import datetime, timedelta
 
 #get useful funcs
 from logic import checkIfLatest , downloadPdf , covidapidb , testcol , old_data_col , latest_data_col
@@ -20,9 +21,10 @@ app.secret_key = 'super secret key'
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-
-
-
+#get key value for districts
+id_dist = None
+with open('id_dist_pair.json', 'r') as fp:
+    id_dist = json.load(fp)
 
 
 @app.route('/test', methods=['GET','POST'])
@@ -64,7 +66,16 @@ def update_trigger():
     return jsonify(resp)
 
 
+@app.route('/ids', methods=['GET'])
+def getKeyValue():
+    return jsonify(id_dist)
 
+@app.route('/history/<id>', methods=['GET'])
+def district_history(id):
+
+    sort_d_history = district_history_util(id)
+
+    return jsonify(sort_d_history)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -72,12 +83,17 @@ def home():
     last_updated = data['date']
     data = data['stats']
     rows = []
+
+    _dist_id = {v: k for k, v in id_dist.items()}
+
     cols = [ 'District/Municipal' , 'TOTAL CASES' , 'NEW CASES' , 'TOTAL DEATHS' , 'NEW DEATHS']
     for d in data.keys():
 
         rows.append( { 'district': d , 'total_cases' : data[d]['TOTAL_CASES'] , 
                     'new_cases' : data[d]['NEW_CASES'] , 'total_deaths' : data[d]['TOTAL_DEATHS'] , 
-                    'new_deaths' : data[d]['NEW_DEATHS']  } )
+                    'new_deaths' : data[d]['NEW_DEATHS'] , 'recovered' : data[d]['RECOVERED'] , 'url' : '/graph/' + _dist_id[d] } )
+
+
 
     print(rows[0])
     print(rows[1])
@@ -85,7 +101,50 @@ def home():
     return render_template('start.html' , rows=rows , cols = cols , last_updated = last_updated)
 
 
+@app.route('/graph/<id>', methods=['GET'])
+def graph(id='1'):
+    data = district_history_util(id)
+    new_cases = []
+    total_cases = []
+    recovered_cases = []
+    for i , d in  enumerate( data ):
+        new_cases.append( { "x" : i , "y" : d['NEW_CASES'] } )
+        total_cases.append( { "x" : i , "y" : d['TOTAL_CASES'] - d['RECOVERED']  } )
+        recovered_cases.append( { "x" : i , "y" : d['RECOVERED'] } )
+    
+    new_cases = json.dumps(new_cases)
+    total_cases = json.dumps(total_cases)
+    recovered_cases = json.dumps(recovered_cases)
 
+    return render_template('line_graph.html' , new_cases = new_cases , total_cases = total_cases , recovered_cases = recovered_cases , dist = id_dist[id])
+
+
+
+def district_history_util(id):
+    district = id_dist[id]
+    data = old_data_col.find({})
+    fourteen_days_date = datetime.today() - timedelta(days=15)
+    d_history = []
+
+    for d in data:
+        stat = d['stats'][district]
+        date = d['date']
+        if date > fourteen_days_date and 'NEW_CASES' in stat.keys():
+
+            for ks in stat.keys():
+                try:
+                    stat[ks] = int( stat[ks] )
+                except:
+                    spl = ((str(stat[ks])).split(' '))[0]
+                    stat[ks] = int(spl)
+
+
+            stat['date'] = date
+            d_history.append(stat)
+
+    sort_d_history = sorted(d_history, key = lambda i: i['date'])
+
+    return sort_d_history
 
 if __name__ == '__main__':
     app.run()
